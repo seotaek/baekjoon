01deedd05dd44ff6a1ee3e7ac984fe4fcc9b384e
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-02-28 13:36:01 +0900

    코딩도장49.1

---
+++ 구조체 공부/structure49.1/structure49.1/structure49.1.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct person {
+	char name[20];
+	int age;
+	char address[100];
+};
+
+int main() {
+	struct person *p1 = malloc(sizeof(struct person));
+
+	strcpy(p1->name, "홍길동");
+	p1->age = 30;
+	strcpy(p1->address, "서울시 용산구 한남동");
+
+	printf("이름: %s\n", p1->name);       
+	printf("나이: %d\n", p1->age);        
+	printf("주소: %s\n", p1->address);
+
+	free(p1);
+	return 0;
+}
