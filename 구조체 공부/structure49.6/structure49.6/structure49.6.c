5facc3b0686805137cdd035edf03c9712b926fbc
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 14:35:05 +0900

    코딩도장49.6

---
+++ 구조체 공부/structure49.6/structure49.6/structure49.6.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Point3D {
+    float x;
+    float y;
+    float z;
+} Point3D;
+
+int main() {
+    Point3D* p1 = malloc(sizeof(Point3D));
+    p1->x = 10.000000f;
+    p1->y = 20.000000f;
+    p1->z = 30.000000f;
+
+    printf("%f %f %f\n", p1->x, p1->y, p1->z);
+
+    free(p1);
+
+    return 0;
+}
