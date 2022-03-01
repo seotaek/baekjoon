0c1510485a66e6a2170e7d60f41472c7bb756380
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 16:38:31 +0900

    코딩도장51.4

---
+++ 구조체 공부/structure51.4/structure51.4/structure51.4.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+struct CompressHeader {
+    char flags;
+    int version;
+};
+
+int main()
+{
+    struct CompressHeader header;
+
+    printf("%d\n", sizeof(header));
+
+    return 0;
+}
