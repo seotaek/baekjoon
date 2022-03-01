42f780d0d0bf9197bce6fe839905d72cb44986ef
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 15:29:53 +0900

    코딩도장50.2

---
+++ 구조체 공부/structure50.2/structure50.2/structure50.2.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Rectangle {
+    int x1, y1;
+    int x2, y2;
+};
+
+int main()
+{
+    struct Rectangle rect;
+    int area;
+
+    rect.x1 = 20;
+    rect.y1 = 20;
+    rect.x2 = 40;
+    rect.y2 = 30;
+
+    int width = abs(rect.x1 - rect.x2);
+    int height = abs(rect.y1 - rect.y2);
+    area = width * height;
+
+    printf("%d\n", area);
+
+    return 0;
+}
