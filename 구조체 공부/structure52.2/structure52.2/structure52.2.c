7a262dcf3401f95d4c98d2a46d40602bad9e5497
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 19:46:47 +0900

    코딩도장52.2

---
+++ 구조체 공부/structure52.2/structure52.2/structure52.2.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <string.h>    // memcpy 함수가 선언된 헤더 파일
+
+struct Point2D {
+    int x;
+    int y;
+};
+
+int main() {
+    struct Point2D* p1 = malloc(sizeof(struct Point2D));
+    struct Point2D* p2 = malloc(sizeof(struct Point2D));
+
+    p1->x = 10;
+    p1->y = 20;
+
+    memcpy(p2, p1, sizeof(struct Point2D));
+
+    printf("%d %d\n", p2->x, p2->y);
+
+    return 0;
+}
