1a7d1c706669c413ee7a62e875d89316baeab169
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 15:56:44 +0900

    코딩도장51.1

---
+++ 구조체 공부/structure51.1/structure51.1/structure51.1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+struct PacketHeader {
+    char flags;    // 1바이트
+    int seq;       // 4바이트
+};
+
+int main() {
+    struct PacketHeader p1;
+
+    printf("%d\n", sizeof(p1.flags));
+    printf("%d\n", sizeof(p1. seq));
+    printf("%d\n", sizeof(p1));
+    printf("%d\n", sizeof(struct PacketHeader));
+
+    return 0;
+}
