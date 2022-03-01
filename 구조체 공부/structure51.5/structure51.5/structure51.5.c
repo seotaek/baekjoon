a3ae26bfa045713095e40d652d68f1de211f0b9c
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 16:40:13 +0900

    코딩도장51.5

---
+++ 구조체 공부/structure51.5/structure51.5/structure51.5.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+#pragma pack(push, 1)
+struct Packet {
+    short length;
+    int seq;
+};
+#pragma pack(pop)
+
+int main()
+{
+    struct Packet pkt;
+
+    printf("%d\n", sizeof(pkt));
+
+    return 0;
+}
