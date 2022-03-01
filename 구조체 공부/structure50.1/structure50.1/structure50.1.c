dfe83a320e729e3805cc49ec69a0cc43b07c375f
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 15:09:14 +0900

    코딩도장50.1

---
+++ 구조체 공부/structure50.1/structure50.1/structure50.1.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>    // sqrt 함수가 선언된 헤더 파일
+
+struct Point2D {
+    int x;
+    int y;
+};
+
+int main() {
+    struct Point2D p1;
+    struct Point2D p2;
+
+    scanf("%d", &p1.x);
+    scanf("%d", &p1.y);
+    scanf("%d", &p2.x);
+    scanf("%d", &p2.y);
+
+    int a = p2.x - p1.x;
+    int b = p2.y - p1.y;
+
+    double c = sqrt((a * a) + (b * b));
+
+    printf("%f\n", c);
+
+    return 0;
+}
