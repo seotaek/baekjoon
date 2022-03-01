2438ef7482f5c06087115fed3ef1a3e2b6b98aec
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 16:09:07 +0900

    코딩도장51.1(2)

---
+++ 구조체 공부/structure51.1(2)/structure51.1(2)/structure51.1(2).c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include<stddef.h>
+
+struct PacketHeader {
+    char flags;    // 1바이트
+    int seq;       // 4바이트
+};
+
+int main() {
+    
+
+    printf("%d\n", offsetof(struct PacketHeader,flags));
+    printf("%d\n", offsetof(struct PacketHeader, seq));
+   
+
+    return 0;
+}
