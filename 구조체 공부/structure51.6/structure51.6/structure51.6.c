92e63fbcaf1c15c571dc1b125dbb8fdadedbdc39
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 16:42:09 +0900

    코딩도장51.6

---
+++ 구조체 공부/structure51.6/structure51.6/structure51.6.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+struct EncryptionHeader {
+    char flags;
+    int x;
+    int y;
+       
+};
+
+int main()
+{
+    struct EncryptionHeader header;
+
+    printf("%d\n", sizeof(header));
+
+    return 0;
+}
