2f047ec82403302926fee16b9dba72e9918e21c1
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-01 14:21:14 +0900

    코딩도장49.3

---
+++ 구조체 공부/structure49.3/structure49.3/structure49.3.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+struct person {    // 구조체 정의
+    char name[20];        // 구조체 멤버 1
+    int age;              // 구조체 멤버 2
+    char address[100];    // 구조체 멤버
+};
+int main() {
+    struct person p1;
+    struct person* ptr;
+
+    ptr = &p1;
+
+    ptr->age = 30;
+    printf("나이:%d\n", p1.age);
+    printf("나이:%d\n", ptr->age);
+
+    return 0;
+}
