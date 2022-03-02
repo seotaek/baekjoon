290820d7e0dfed5596c71b433988b39b03d3d776
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-02 22:48:11 +0900

    코딩도장 54.1 공용체

---
+++ 구조체 공부/structure54.1/structure54.1/structure54.1.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <string.h>
+
+union Box {
+	short candy;
+	float snack;
+	char doll[8];
+};
+
+int main() {
+	union Box b1;
+
+	printf("%d\n", sizeof(b1));//공용체의 전체 크기는 가장 큰 자료형의 크기
+
+	strcpy(b1.doll, "bear");
+
+	printf("%d\n", b1.candy);
+	printf("%f\n", b1.snack);
+	printf("%s\n", b1.doll);
+
+	return 0;
+}
