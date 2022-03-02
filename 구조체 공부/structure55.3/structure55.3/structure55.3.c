619c62071b09fe2cdd3a61e5c62c878b937c2cf9
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-03 00:07:28 +0900

    코딩도장55.3

---
+++ 구조체 공부/structure55.3/structure55.3/structure55.3.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+struct Vector3 {
+	union {        // 익명 공용체
+		struct {   // 익명 구조체
+			float x;
+			float y;
+			float z;
+
+		};
+		float v[3]; //좌표를 배열로 저장
+	};
+};
+
+int main() {
+	struct Vector3 pos;
+	for (int i = 0; i < 3; i++)
+		pos.v[i] = 1.0f;
+	printf("%f %f %f\n", pos.x, pos.y, pos.z);
+
+	return 0;
+}
+
