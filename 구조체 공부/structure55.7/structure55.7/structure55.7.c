fcbfd20dcbe75e9ecb80709b949318d31a700fe3
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-03 13:59:25 +0900

    코딩도장55.7

---
+++ 구조체 공부/structure55.7/structure55.7/structure55.7.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+struct DeviceOption {
+    union {
+        unsigned long long option;
+        struct {
+            char boot[4];
+            char interrupt[2];
+            char bus[2];
+        };
+    };
+};
+
+int main()
+{
+    struct DeviceOption opt;
+
+    opt.boot[0] = 0x01;
+    opt.boot[1] = 0x02;
+    opt.boot[2] = 0x03;
+    opt.boot[3] = 0x04;
+    opt.interrupt[0] = 0x05;
+    opt.interrupt[1] = 0x06;
+    opt.bus[0] = 0x07;
+    opt.bus[1] = 0x11;
+
+    printf("0x%llx\n", opt.option);
+
+    return 0;
+}
