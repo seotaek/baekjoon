6e7c7733a33293a66e40650c371dc0dd9bce4809
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-04 17:04:14 +0900

    코딩도장59.7

---
+++ 구조체 공부/structure59.7/structure59.7/structure59.7.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int numArr[5] = { 11, 22, 33, 44, 55 };
+    int* numPtrA;
+    void* ptr;
+
+    numPtrA = &numArr[2];
+    ptr = numArr;
+
+    printf("%d\n",      *(2+numPtrA));
+    printf("%d\n",        *((int*)ptr+1));
+
+    return 0;
+}
