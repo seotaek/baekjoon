4c74f9295be171f2fd93136edf499691cf150381
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-04 14:57:21 +0900

    코딩도장57.10

---
+++ 구조체 공부/structure57.10/structure57.10/structure57.10.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+typedef enum _INTERFACE_TYPE {
+    InterfaceTypeUndefined = -1,
+    Internal,
+    Isa,
+    Eisa,
+    MicroChannel,
+    TurboChannel,
+    PCIBus,
+    VMEBus,
+    NuBus,
+    PCMCIABus,
+    CBus,
+    MPIBus,
+    MPSABus,
+    ProcessorInternal,
+    InternalPowerBus,
+    PNPISABus,
+    PNPBus,
+    MaximumInterfaceType
+} INTERFACE_TYPE, * PINTERFACE_TYPE;
+
+int main()
+{
+    for(INTERFACE_TYPE i=Internal;i< MaximumInterfaceType;i++)
+    {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
