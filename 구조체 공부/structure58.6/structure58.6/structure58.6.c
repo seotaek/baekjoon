29de4f2df0c76140a54d5c30913f5ad5fdd93a23
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-04 15:48:22 +0900

    코딩도장58.6

---
+++ 구조체 공부/structure58.6/structure58.6/structure58.6.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int base = 21;
+    int height = 13;
+    float area;
+    area = (float)base * height * 0.5;
+    printf("%f\n", area);
+
+    return 0;
+}
