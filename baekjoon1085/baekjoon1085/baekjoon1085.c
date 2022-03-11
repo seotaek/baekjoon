c62cee0fdf5e6aca27871f5742570ff8e09a78d9
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-11 21:09:35 +0900

    백준1085

---
+++ baekjoon1085/baekjoon1085/baekjoon1085.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int x, y, w, h;
+	scanf("%d %d %d %d", &x, &y, &w, &h);
+	int min = x;
+	if (min > y)
+		min = y;
+	if (min > h - y)
+		min = h - y;
+	if (min > w - x)
+		min = w - x;
+	printf("%d\n", min);
+	return 0;
+}
