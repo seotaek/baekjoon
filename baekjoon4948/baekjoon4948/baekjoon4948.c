eea1190cf679185361fd3fc12085ef5a23105d00
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-11 20:24:09 +0900

    백준4948

--- baekjoon4948/baekjoon4948/baekjoon4948.c
+++ baekjoon4948/baekjoon4948/baekjoon4948.c
@@ -1,29 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main() {
 	int n;
-	int count;
-	int flag;
 	while (1) {
-		count = 0;
 		scanf("%d", &n);
 		if (n == 0)
 			break;
-		for (int i = n+1; i <= 2*n; i++) {
-			flag = 0;
-			if (i == 2) {
-				flag = 0;
-				continue;
-			}
-			for (int j = 2; j < i; j++) {
-				if (i % j == 0)
-					flag = 1;
-			}
-			if (flag == 0)
+		int* s1 = malloc(sizeof(int) * 246913);
+		for (int i = 0; i < 246913; i++)
+			s1[i] = 0;
+		s1[0] = 1;
+		s1[1] = 1;
+		for (int i = 2; i <= n*2; i++) {
+			for (int j = 2; i * j <= n*2; j++)
+				s1[i * j] = 1;
+		}
+		int count = 0;
+		for (int i = n + 1; i <= 2 * n; i++) {
+			if (s1[i] == 0)
 				count++;
 		}
 		printf("%d\n", count);
+		free(s1);
 	}
 	return 0;
 }
