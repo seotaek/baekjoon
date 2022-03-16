57e5cc4cc0a93021f42d3f35ae1973e992b12bfd
gprua123@office.kw.ac.kr <gprua123@office.kw.ac.kr>
2022-03-16 22:27:49 +0900

    백준1018

--- baekjoon1018/baekjoon1018/1018.c
+++ baekjoon1018/baekjoon1018/1018.c
@@ -1,37 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 int main() {
 	int n, m;
 	scanf("%d %d", &n, &m);
-	char **s1 = malloc(sizeof(char*) * m);
-	for (int i = 0; i < m; i++) {
-		s1[i] = malloc(sizeof(char) * n);
-	}
+
+	char s1[51][51];
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%c", &s1[i][j]);
-		}
+		scanf("%s", &s1[i]);
 		
 	}
+
+
 	int count = 0;
-	for (int i = 0; i < n-1; i++) {
-		for (int j = 0; j < m-1; j++) {
-			if (s1[i][j] == s1[i + 1][j]) {
-				count++;
-				
+	int min = 64;
+	for (int i = 0; i < n - 7; i++) {
+		for (int j = 0; j < m - 7; j++) {
+			for (int k = i; k < i + 8; k++) {
+				if (k % 2 == 0) {
+					for (int t = j; t < j + 8; t++) {
+						if (t % 2 == 0 && s1[k][t] != 'B')
+							count++;
+						else if (t % 2 != 0 && s1[k][t] != 'W')
+							count++;
+					}
+				}
+				else if(k%2!=0) {
+					for (int t = j; t < j + 8; t++) {
+						if (t % 2 == 0 && s1[k][t] != 'W')
+							count++;
+						else if (t % 2 != 0 && s1[k][t] != 'B')
+							count++;
+					}
+				}
+
 			}
-			else if (s1[i][j] == s1[i][j + 1])
-				count++;
+			if (count > 32)
+				count = 64 - count;
+			if (min > count)
+				min = count;
+			count = 0;
 		}
 	}
-	printf("%d\n", count);
+	printf("%d\n", min);
 
-	for (int i = 0; i < m; i++) {
-		free(s1[i]);
-	}
-	free(s1);
 
 	return 0;
 }
